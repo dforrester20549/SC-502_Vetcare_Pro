e51dc4b2d313d6be04c76afe3561f8e5f05425f0
yeilansalas <salasyeilan9@gmail.com>
2024-11-18 22:54:42 -0600

    actualizacion de vistas home
    se cambiaron las vistas

--- View/Home/servicios.php
+++ View/Home/servicios.php
@@ -1,124 +1,126 @@
 <?php
     include_once '../_Layout_Externo.php';
 ?>
-    <main> 
-        <!-- Hero Area Start -->
-        <div class="slider-area2 slider-height2 d-flex align-items-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="hero-cap text-center pt-50">
-                            <h2>Services</h2>
-                        </div>
+<main> 
+    <!-- Área Principal de Inicio -->
+    <div class="slider-area2 slider-height2 d-flex align-items-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="hero-cap text-center pt-50">
+                        <h2>Servicios</h2>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Hero Area End -->
-        <!--? Our Services Start -->
-        <div class="our-services section-padding30">
-            <div class="container">
-                <div class="row justify-content-sm-center">
-                    <div class="cl-xl-7 col-lg-8 col-md-10">
-                        <!-- Section Tittle -->
-                        <div class="section-tittle text-center mb-70">
-                            <span>Our Professional Services</span>
-                            <h2>Best Pet Care Services</h2>
-                        </div> 
+    </div>
+    <!-- Fin del Área Principal de Inicio -->
+
+    <!--? Nuestros Servicios Inicio -->
+    <div class="our-services section-padding30">
+        <div class="container">
+            <div class="row justify-content-sm-center">
+                <div class="cl-xl-7 col-lg-8 col-md-10">
+                    <!-- Título de la sección -->
+                    <div class="section-tittle text-center mb-70">
+                        <span>Nuestros Servicios Profesionales</span>
+                        <h2>Mejores Servicios de Cuidado de Mascotas</h2>
+                    </div> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6">
+                    <div class="single-services text-center mb-30">
+                        <div class="services-ion">
+                            <span class="flaticon-animal-kingdom"></span>
+                        </div>
+                        <div class="services-cap">
+                            <h5><a href="#">Alojamiento para Mascotas</a></h5>
+                            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                <span class="flaticon-animal-kingdom"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Pet Boarding</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
+                <div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="single-services text-center mb-30">
+        <div class="services-ion">
+             <span class="flaticon-animals"></span>
+        </div>
+        <div class="services-cap">
+            <h5><a href="#">Tratamiento para Mascotas</a></h5>
+            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="single-services text-center mb-30">
+        <div class="services-ion">
+           <span class="flaticon-animals-1"></span>
+        </div>
+        <div class="services-cap">
+            <h5><a href="#">Vacunaciones</a></h5>
+            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="single-services text-center mb-30">
+        <div class="services-ion">
+             <span class="flaticon-animals"></span>
+        </div>
+        <div class="services-cap">
+            <h5><a href="#">Tratamiento para Mascotas</a></h5>
+            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="single-services text-center mb-30">
+        <div class="services-ion">
+           <span class="flaticon-animals-1"></span>
+        </div>
+        <div class="services-cap">
+            <h5><a href="#">Vacunaciones</a></h5>
+            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+    <div class="single-services text-center mb-30">
+        <div class="services-ion">
+            <span class="flaticon-animal-kingdom"></span>
+        </div>
+        <div class="services-cap">
+            <h5><a href="#">Alojamiento para Mascotas</a></h5>
+            <p>Consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+</div>
+<!-- Fin de nuestros servicios -->
+
+<!--? Contacto para propietarios de mascotas inicio -->
+<div class="contact-animal-owner section-bg" data-background="assets/img/gallery/section_bg04.png">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="contact_text text-center">
+                    <div class="section_title text-center">
+                        <h3>¡Puedes llamarnos en cualquier momento!</h3>
+                        <p>Porque sabemos que incluso la mejor tecnología es tan buena como las personas que la respaldan. Soporte técnico 24/7.</p>
                     </div>
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                 <span class="flaticon-animals"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Pet Treatment</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                               <span class="flaticon-animals-1"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Vaccinations</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                 <span class="flaticon-animals"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Pet Treatment</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                               <span class="flaticon-animals-1"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Vaccinations</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class=" col-lg-4 col-md-6 col-sm-6">
-                        <div class="single-services text-center mb-30">
-                            <div class="services-ion">
-                                <span class="flaticon-animal-kingdom"></span>
-                            </div>
-                            <div class="services-cap">
-                                <h5><a href="#">Pet Boarding</a></h5>
-                                <p>consectetur adipiscing elit, serfd dgo eiusmod tempor incididunt ut ore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
+                    <div class="contact_btn d-flex align-items-center justify-content-center">
+                        <a href="contact.html" class="btn white-btn">Contáctanos</a>
+                        <p>O<a href="#"> +880 4664 216</a></p>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Our Services End -->
-        <!--? contact-animal-owner Start -->
-        <div class="contact-animal-owner section-bg" data-background="assets/img/gallery/section_bg04.png">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <div class="contact_text text-center">
-                            <div class="section_title text-center">
-                                <h3>Any time you can call us!</h3>
-                                <p>Because we know that even the best technology is only as good as the people behind it. 24/7 tech support.</p>
-                            </div>
-                            <div class="contact_btn d-flex align-items-center justify-content-center">
-                                <a href="contact.html" class="btn white-btn">Contact Us</a>
-                                <p>Or<a href="#"> +880 4664 216</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- contact-animal-owner End -->
-    </main>
+    </div>
+</div>
+<!-- Fin contacto propietario de mascotas -->
+
     
     <?php
         include_once '../_Footer.php';
